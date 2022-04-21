597161659c2e0d753e81a1cfc4c729eff2eee295
Dell <Dell@DELL>
2022-04-21 10:35:03 +0530

    basic eclipse project

---
+++ GitHubProjects/src/main/java/com/basicCode/Addition.java
@@ -0,0 +1,13 @@
+package com.basicCode;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b= 8;
+		System.out.println("Additiom of two numbers is: "+(a+b));
+				
+	}
+
+}
---
+++ GitHubProjects/src/main/java/com/basicCode/HelloWorldExample.java
@@ -0,0 +1,11 @@
+package com.basicCode;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi I am Kamini");
+		System.out.println("Welcome");
+	}
+
+}
